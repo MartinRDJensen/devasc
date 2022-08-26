d79021cff832a870702beac525f13f734a0f9801
Martin Jensen <m.rd.jensen42@gmail.com>
2022-08-26 20:40:33 +0200

    Fixes the git push script

--- pusher.sh
+++ pusher.sh
@@ -2,7 +2,7 @@ die () {
     echo >&2 "$@"
     exit 1
 }
-[ "$#" -eq 1 ] || die "1 argument required, &# provided"
+[ "$#" -eq 1 ] || die "1 argument, the commit message, required, $# provided"
 git add .;
 git commit -m "$1";
 git push;
